f946cfbda4830fcbad2b20024937d61a9ffc4a91
AbdoulayeLah <lah.telecoms@gmail.com>
2022-04-25 10:27:53 +0000

    fin

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+
+from random import randint
 t=2
+
 while t==2:
+
     def alaligne():
         print()
 
-    from random import randint
+    
     alaligne()
     print("-------LE JUSTE PRIX-------")
     alaligne()
@@ -13,48 +17,47 @@ while t==2:
     saisie=input("Essaie : ")
 
     def test():
-      global saisie
-      test=saisie.isnumeric()
-      while test==False: 
-        print("Erreur de saisie")
-        saisie=input("non,c'est pas cà : ")
+        global saisie
         test=saisie.isnumeric()
-        saisie=int(saisie)
-        return saisie
+        while test==False: 
+            saisie=input("T'es con ou quoi, j'ai dit un nombre merde! : ")
+            test=saisie.isnumeric()
+            saisie=int(saisie)
+            return saisie
 
     
 
     test()
     note=3
     i=1
+    
+
     while i<3:
-        if saisie<num:
+        saisie=int(saisie)
+        if (saisie)<(num):
             print("C'est plus")
             note=note-1
-            saisie=input("non,c'est pas cà toi aussi : ")
+            saisie=input("Réessaie : ")
             test()
-        elif saisie>num:
+        elif (saisie)>(num):
             print("C'est moins")
             note=note-1
-            saisie=input("non,c'est pas cà toi aussi : ")
+            saisie=input("Réessaie : ")
             test()
-       else :
-        print(f"Victoire vous avez {note} points")
+        else :
+            print(f"Tu l'asfais mec, t'es le meilleur. T'as {note} points")
+            alaligne()
+            print("-------LE JEU EST FINI-------")
+            alaligne()
+            break
+    
+        i=i+1
+    
+
+    if note==1:
+        p=note-1
+        print(f"T'es un looser, tu m'as fais perdre mon temps. T'as eu {p} point ")
         alaligne()
         print("-------LE JEU EST FINI-------")
         alaligne()
-        break
 
-    i=i+1
-
-    
-
-    
-if note==1:
-    p=note-1
-    print(f"T'es un looser, t'as eu {p} point minable ")
-    alaligne()
-    print("-------LE JEU EST FINI-------")
-    alaligne()
-
-#modifier par le stagiaire pour apprendre a gérer les conflit
