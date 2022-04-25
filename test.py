cf72886b5a435662fe30c094041b81281530ed9f
AbdoulayeLah <lah.telecoms@gmail.com>
2022-04-25 08:08:30 +0000

    amélioration code 1

--- test.py
+++ test.py
@@ -1,45 +1,54 @@
 from random import randint
+print()
+print("-------LE JUSTE PRIX-------")
+print()
 
-prix=0
-prix=randint(1,100)
-i=1
+num=randint(1,100)
+
+
 print("Tu as droit à 5 essaies entre 1 et 100")
-saisie=input("Entrer un nombre : ")
+saisie=input("Entre un nombre : ")
 
 def test():
     global saisie
-    v=True
-    v=saisie.isnumeric()
-    while v==False: 
+    test=saisie.isnumeric()
+    while test==False: 
         print("Erreur de saisie")
         saisie=input("Entrer un nombre : ")
-        v=saisie.isnumeric()
+        test=saisie.isnumeric()
     saisie=int(saisie)
     return saisie
 
     
 test()
-print(saisie)
 note=5
-
-while i<=4:
+i=1
+while i<5:
     
-    if saisie<prix:
+    if saisie<num:
         print("C'est plus")
         note=note-1
         saisie=input("Entrer un nombre : ")
         test()
-    elif saisie>prix:
+    elif saisie>num:
         print("C'est moins")
         note=note-1
         saisie=input("Entrer un nombre : ")
         test()
     else :
         print(f"Win vous avez {note} points")
+        print()
+        print("-------LE JEU EST FINI-------")
+        print()
         break
+
+    i+=1
     
-    i=i+1
+
     
 if note==1:
     p=note-1
     print(f"Echec vous avez {p} points")
+    print()
+    print("-------LE JEU EST FINI-------")
+    print()
